860cb9dd8405f8f05ccbaa0f4c48fdde7249f3c3
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-04-01 20:56:54 +0200

    Update: Added the register method, updated the docs.

--- clients/dart/switchkeys/switchkeys.dart
+++ clients/dart/switchkeys/switchkeys.dart
@@ -1,18 +1,6 @@
-import 'package:switchkeys/src/api/models/auth.dart';
+import 'package:switchkeys/examples/auth.dart';
 
-class SwitchKeys {
-  static SwitchKeyAuth auth(String apiKey) => SwitchKeyAuth(apiKey: apiKey);
-}
-
-void main() async {
-  // Provide your API key here
-  String apiKey = "your_api_key";
-
-  // Get an instance of SwitchKeyAuth
-  SwitchKeyAuth auth = SwitchKeys.auth(apiKey);
-
-  // Now you can use the auth instance to make API calls
-  var user = await auth.login(email: "admin@gmasil.com", password: "0000");
-  print("user: ");
-  print(user.email);
+main() {
+  // Load the auth example.
+  authMain();
 }

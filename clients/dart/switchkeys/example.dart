b50214e6993f01bc1d8a210f84198bdb8af56879
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-04-07 15:18:53 +0200

    Publish: Publish the dart package.

--- clients/dart/switchkeys/example.dart
+++ clients/dart/switchkeys/example.dart
@@ -1,14 +1,14 @@
-// import 'package:switchkeys/examples/auth.dart';
-// import 'package:switchkeys/examples/organization.dart';
-// import 'package:switchkeys/examples/projects.dart';
-import 'package:switchkeys/examples/environments.dart';
+// // import 'package:switchkeys/examples/auth.dart';
+// // import 'package:switchkeys/examples/organization.dart';
+// // import 'package:switchkeys/examples/projects.dart';
+// import 'package:switchkeys/examples/environments.dart';
 
-main() {
-  // Load the auth module examples.
-  // authMain();
+// main() {
+//   // Load the auth module examples.
+//   // authMain();
 
-  // Load the organization module examples
-  // organizationsMain();
-  // projectsMain();
-  environmentsMain();
-}
+//   // Load the organization module examples
+//   // organizationsMain();
+//   // projectsMain();
+//   environmentsMain();
+// }

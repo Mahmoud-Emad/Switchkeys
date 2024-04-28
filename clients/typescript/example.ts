ee448c56f4f1b70ea6100f739481201df18f2d89
Thunder <mahmmoud.hassanein@gmail.com>
2024-04-28 14:22:20 +0300

    Update: Added some examples to test the workflow. (#7)

--- clients/typescript/example.ts
+++ clients/typescript/example.ts
@@ -1,91 +1,14 @@
-// Import the SwitchKeys class from the client library.
-// import SwitchKeys from "switchkeys-ts-client";
-import SwitchKeys from "./switchkeys/core/base";
+import { authExample } from "./examples/auth.example";
+import { organizationExample } from "./examples/organizations.example";
+import { projectExample } from "./examples/projects.example";
 
 /**
  * Main function demonstrating the usage of the SwitchKeys client.
  */
 async function main() {
-  // Create an instance of the SwitchKeys client.
-  const switchkeys = new SwitchKeys();
-
-  // Uncomment and replace placeholders with actual data to register a new member.
-  /*
-  const newMember = await switchkeys.auth.register({
-    email: "<member_email>",
-    firstName: "<member_first_name>",
-    lastName: "<member_last_name>",
-    password: "<password>",
-  });
-  console.log(`New member registered. Access token: ${newMember.accessToken}`);
-  */
-
-  // Uncomment and replace placeholders with actual credentials to log in an existing member.
-  /*
-  const loggedInMember = await switchkeys.auth.login({
-    email: "<member_email>",
-    password: "<password>",
-  });
-  console.log(`Member logged in. Access token: ${loggedInMember.accessToken}`);
-  */
-
-  // Uncomment and replace the ID placeholder with the actual member ID to retrieve member data by ID.
-  /*
-  const memberId = 9; // Replace with the actual member ID
-  const memberByID = await switchkeys.organizations.members.getById(memberId);
-  console.log(`Member data retrieved by ID. First name: ${memberByID.firstName}`);
-  */
-
-  // Uncomment and replace the email placeholder with the actual member email to retrieve member data by email.
-  /*
-  const memberEmail = "john@example.com"; // Replace with the actual member email
-  const memberByEmail = await switchkeys.organizations.members.getByEmail(memberEmail);
-  console.log(`Member data retrieved by email. First name: ${memberByEmail.firstName}`);
-  */
-
-  // Uncomment and replace the memberId placeholder with the actual member ID to update member data.
-  // Use the login method before updating the user tokens.
-  /*
-  await switchkeys.auth.login({ email: "admin@gmail.com", password: "0000" });
-  const memberId = 9; // Replace with the actual member ID
-  const member = await switchkeys.organizations.members.getById(memberId);
-  console.log(`Member data retrieved by ID. First name: ${member.firstName}`);
-  */
-
-  // Uncomment and replace the data placeholder with actual organization data to create a new organization.
-  /*
-  const organization = await switchkeys.organizations.create({ name: "Test" });
-  console.log(`Organization ID: ${organization.id}`);
-  */
-
-  // Uncomment and replace the organization ID placeholder with the actual ID to get organization data by ID.
-  /*
-  const organization = await switchkeys.organizations.getById(5);
-  console.log(`Organization name: ${organization.name}`);
-  */
-
-  // Uncomment and replace the organization ID placeholder with the actual ID to delete an organization.
-  /*
-  await switchkeys.organizations.delete(5); // Returns null or error
-  */
-
-  // Uncomment, update the organization ID, and run to demonstrate organization update.
-  /*
-  let organization = await switchkeys.organizations.getById(6);
-  console.log(`Organization name: ${organization.name}`);
-  organization = await switchkeys.organizations.update(6, { name: "Test 2" });
-  console.log(`Organization name: ${organization.name}`);
-  */
-
-  // Uncomment and replace placeholders with actual data to demonstrate project-related operations.
-  /*
-  const organizationId = 6; // Replace with the actual organization ID
-  const projects = await switchkeys.organizations.projects.all(organizationId);
-  console.log(`First project name: ${projects[0].name}`);
-  */
-
-  // Add more project-related operations here...
+  await organizationExample();
+  await projectExample();
+  await authExample();
 }
 
-// Call the main function to execute the example.
 main();

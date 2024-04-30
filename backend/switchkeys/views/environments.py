683d2c44572f22a19858f4812b204ab039bd17e4
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-04-30 16:01:15 +0300

    Feat: Implemented the environment update/delete API endpoints.

--- backend/switchkeys/views/environments.py
+++ backend/switchkeys/views/environments.py
@@ -103,62 +103,73 @@ class ProjectEnvironmentApiView(GenericAPIView):
             message="The project environment found.",
         )
 
-    # def put(self, request: Request, environment_id: str) -> CustomResponse:
-    #     """Update an project environment by its ID."""
-    #     environment = get_environment_by_id(environment_id)
+    def put(self, request: Request, environment_id: str) -> CustomResponse:
+        """Update an project environment by its ID."""
+        environment = get_environment_by_id(environment_id)
 
-    #     if environment is None:
-    #         return CustomResponse.not_found(
-    #             message="The project environment does not exist."
-    #         )
+        if environment is None:
+            return CustomResponse.not_found(
+                message="The project environment does not exist."
+            )
 
-    #     data = request.data
-    #     serializer = self.get_serializer(environment, data=data)
-    #     if serializer.is_valid():
-    #         project_id: int = serializer.validated_data.get("project_id")
-    #         project: OrganizationProject = get_project_by_id(str(project_id))
+        data = request.data
+        serializer = self.get_serializer(environment, data=data)
+        if serializer.is_valid():
+            project_id: int = serializer.validated_data.get("project_id")
+            project: OrganizationProject = get_project_by_id(str(project_id))
 
-    #         if project is None:
-    #             return CustomResponse.not_found(
-    #                 message="Project not found.",
-    #             )
+            if project is None:
+                return CustomResponse.not_found(
+                    message="Project not found.",
+                )
 
-    #         if request.user.id != project.organization.owner.id:
-    #             return CustomResponse.unauthorized(
-    #                 message="You do not have permission to access this resource because you are not the creator of the organization that owns this project.",
-    #             )
+            if request.user.id != project.organization.owner.id:
+                return CustomResponse.unauthorized(
+                    message="You do not have permission to access this resource because you are not the creator of the organization that owns this project.",
+                )
 
-    #         serializer.save()
-    #         return CustomResponse.success(
-    #             data=serializer.data,
-    #             message="Organization project environment has been updated successfully.",
-    #             status_code=201,
-    #         )
+            environment_name: str = serializer.validated_data.get("name")
+            is_environment_exist = validate_unique_environment_name(
+                environment_name, project
+            )
 
-    #     return CustomResponse.bad_request(
-    #         data=ProjectEnvironmentSerializer(environment).data,
-    #         message="Please make sure that you entered a valid data..",
-    #         error=serializer.errors,
-    #     )
+            if is_environment_exist:
+                return CustomResponse.bad_request(
+                    error=unique_field_error("name"),
+                    message=f"It seems you've already created a '{environment_name}' environment for this project. Please remove the existing one before creating a new one, or consider changing the name to avoid duplication."
+                )
 
-    # def delete(self, request: Request, environment_id: str) -> Response:
-    #     """Delete an project environment by its ID."""
-    #     environment = get_environment_by_id(environment_id)
+            serializer.save()
+            return CustomResponse.success(
+                data=serializer.data,
+                message="Organization project environment has been updated successfully.",
+                status_code=201,
+            )
 
-    #     if environment is None:
-    #         return CustomResponse.not_found(
-    #             message="The project environment does not exist."
-    #         )
+        return CustomResponse.bad_request(
+            data=ProjectEnvironmentSerializer(environment).data,
+            message="Please make sure that you entered a valid data..",
+            error=serializer.errors,
+        )
 
-    #     project: OrganizationProject = environment.project
-    #     if request.user.id != project.organization.owner.id:
-    #         return CustomResponse.unauthorized(
-    #             message="You do not have permission to access this resource because you are not the creator of the organization that owns this project.",
-    #         )
+    def delete(self, request: Request, environment_id: str) -> CustomResponse:
+        """Delete an project environment by its ID."""
+        environment = get_environment_by_id(environment_id)
 
-    #     environment.delete()
-    #     return CustomResponse.success(
-    #         data={},
-    #         message="Project has been updated successfully.",
-    #         status_code=204,
-    #     )
+        if environment is None:
+            return CustomResponse.not_found(
+                message="The project environment does not exist."
+            )
+
+        project: OrganizationProject = environment.project
+        if request.user.id != project.organization.owner.id:
+            return CustomResponse.unauthorized(
+                message="You do not have permission to access this resource because you are not the creator of the organization that owns this project.",
+            )
+
+        environment.delete()
+        return CustomResponse.success(
+            data={},
+            message="Project has been updated successfully.",
+            status_code=204,
+        )

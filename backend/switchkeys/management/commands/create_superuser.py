479041f24c5ea0621f627852784c58dc5dcedc2c
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-05-23 14:12:35 +0300

    fix: Fixed an issue with building the backend container.

---
+++ backend/switchkeys/management/commands/create_superuser.py
@@ -0,0 +1,23 @@
+import os
+from django.core.management.base import BaseCommand
+from switchkeys.models.users import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        """Use this command only in development mode."""
+        superuser_email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        superuser_password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        if not User.objects.filter(email=superuser_email).exists():
+            User.objects.create_superuser(
+                email=superuser_email,
+                password=superuser_password,
+            )
+            print(f'Superuser with email {superuser_email} created.')
+        else:
+            print(f'Superuser with email {superuser_email} already exists.')
+
+
+
+
+
--- docker/scripts/backend/backend.sh
+++ docker/scripts/backend/backend.sh
@@ -1,22 +1,41 @@
 #!/bin/sh
 
-if [ "$ENV" = "production" ]; then
-    echo "Running in production mode."
-    echo "Waiting for PostgreSQL to be running on: $DATABASE_HOST"
+set -e
 
+# Function to wait for PostgreSQL
+wait_for_postgres() {
+    echo "Waiting for PostgreSQL to be running on: $DATABASE_HOST"
     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
         sleep 0.1
     done
-
     echo "PostgreSQL started"
-else
-    echo "Running in development mode."
+}
+
+# Function to run common Django commands
+run_django_commands() {
     cd backend
     poetry run python3 manage.py flush --no-input
     echo "Running the migration"
     poetry run python3 manage.py makemigrations
     echo "Running the migrate"
     poetry run python3 manage.py migrate
+}
+
+# Function to create superuser if not exists
+create_superuser_if_not_exists() {
+    echo "Checking if superuser exists..."
+    poetry run python3 manage.py create_superuser
+}
+
+# Main script execution
+if [ "$ENV" = "production" ]; then
+    echo "Running in production mode."
+    wait_for_postgres
+    run_django_commands
+    create_superuser_if_not_exists
+else
+    echo "Running in development mode."
+    run_django_commands
     echo "Creating a superuser email: $DJANGO_SUPERUSER_EMAIL"
     poetry run python3 manage.py createsuperuser --noinput
 fi
